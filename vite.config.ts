b7b03cffc352aacdf79012d0c92d145c338183d4
Himenon <k.himeno314@gmail.com>
2022-05-06 19:01:28 +0900

    chore: update

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import * as path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import pkg from "./package.json"
+
+
+const VERSION = pkg.dependencies["@himenon/openapi-typescript-code-generator"];
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
-    "process.env.GENERATOR_VERSION": JSON.stringify("dummy"),
+    "process.env.GENERATOR_VERSION": JSON.stringify(VERSION),
   },
   resolve: {
     alias: {
