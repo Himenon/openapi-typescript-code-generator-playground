f324e87f4c12e3a988b90bb1641f6d23aba53643
K.Himeno <k.himeno314@gmail.com>
2022-05-07 11:37:53 +0900

    fix: base url (#10)

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import * as path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import * as Meta from "@himenon/openapi-typescript-code-generator/meta";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {},
+  base: `/v${Meta.Version}/`,
   resolve: {
     alias: {
       path: "path-browserify",
