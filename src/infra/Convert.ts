8c282e53413fe7de041fef55ffa5f80485f20778
K.Himeno <6715229+Himenon@users.noreply.github.com>
2026-05-06 13:51:53 +0900

    feat: v2.0.3 (#26)

--- src/infra/Convert.ts
+++ src/infra/Convert.ts
@@ -1,6 +1,6 @@
-import * as Api from "@himenon/openapi-typescript-code-generator/dist/api";
-import * as Templates from "@himenon/openapi-typescript-code-generator/dist/templates";
-import type * as Types from "@himenon/openapi-typescript-code-generator/dist/types";
+import * as Api from "@himenon/openapi-typescript-code-generator/api";
+import * as Templates from "@himenon/openapi-typescript-code-generator/templates";
+import type * as Types from "@himenon/openapi-typescript-code-generator/types";
 import * as yaml from "js-yaml";
 
 const EOL = "\n";
@@ -35,7 +35,7 @@ const generateTypeScriptCode = ({ schema, entryPoint, codeGeneratorKind }: Param
     const statements = parser.getOpenApiTypeDefinitionStatements();
     generatorTemplates.forEach((generatorTemplate) => {
       const payload = parser.getCodeGeneratorParamsArray();
-      const extraStatements = Api.TsGenerator.Utils.convertIntermediateCodes(generatorTemplate.generator(payload, generatorTemplate.option));
+      const extraStatements = generatorTemplate.generator(payload, generatorTemplate.option);
       statements.push(...extraStatements);
     });
     return statements;
